2d31e1504741dccf004d655b1f898829ed1d0bd2
iahta <153889016+iahta@users.noreply.github.com>
2025-01-13 15:00:12 -0600

    added draw_move

--- cell.py
+++ cell.py
@@ -13,19 +13,32 @@ class Cell():
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(x1, y1), Point(x2, y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
+        line = Line(Point((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2 ), 
+                    Point((to_cell._x1 + to_cell._x2) // 2, (to_cell._y1 + to_cell._y2) // 2)
+                    )
+        fill_color = "gray" if undo else "red"
+        self._win.draw_line(line, fill_color)
+
+
